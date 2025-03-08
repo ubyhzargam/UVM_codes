b4da2488bf455d4e0d1d7d6f653ef2f73c7226c5
Uby H <136114596+ubyhzargam@users.noreply.github.com>
2025-03-08 22:58:57 +0530

    Create uvm_verbosity_through_id.sv

---
+++ uvm_verbosity_through_id.sv
@@ -0,0 +1,28 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class driver extends uvm_driver;
+  `uvm_component_utils(driver)
+  
+  function new(string path, uvm_component parent);
+    super.new(path,parent);
+  endfunction
+  
+  task run();
+    `uvm_info("DRV1","Executed DRV1",UVM_HIGH);
+    `uvm_info("DRV2","Executed DRV2",UVM_HIGH);
+  endtask
+  
+endclass
+
+module tb;
+  
+  driver drv;
+  
+  initial
+    begin
+      drv=new("DRV",null);
+      drv.set_report_id_verbosity("DRV1",UVM_HIGH);
+      drv.run();
+    end
+endmodule
